2d9aee1817d4ea9f5ab5ef7026a5b4d52ade9fe0
husengg678 <recapzero@gmail.com>
2025-07-07 15:50:14 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 function showMessage() {
   const name = document.getElementById("name").value;
-  document.getElementById("output").innerText =
-    "Terima Kasih, " + name + " Telah Mengisi Form";
-  return false;
+  const tujuan = document.getElementById("tujuan").value;
+  const tanggal = document.getElementById("tanggal").value;
+  const info = document.getElementById("info").value;
+
+  const output = `
+    <strong>Data Terkirim:</strong><br>
+    Nama: ${name}<br>
+    Tujuan: ${tujuan}<br>
+    Tanggal: ${tanggal}<br>
+    Informasi Tambahan: ${info}
+  `;
+
+  document.getElementById("output").innerHTML = output;
+
+  return false; // Supaya tidak reload halaman
 }
